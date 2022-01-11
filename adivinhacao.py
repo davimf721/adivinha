af5f5db1c9d62676374b96975bd062786da6579c
Davi Moreira Fuzatto <davimoreiraf@gmail.com>
2022-01-11 19:50:26 -0400

    Melhorando a lógica e a sintaxe

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,15 +4,18 @@ print("**********************************")
 print("Bem vindos ao jogo de adivinhação!")
 print("**********************************")
 
-numero_secreto = random.randint(1, 10)
+numero_secreto = random.randint(1, 100)
 total_de_tentativas = 3
-rodada = 1
 
-while rodada <= total_de_tentativas:
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
-    print("Você digitou", chute_str)
+    chute_str = input("Digite o seu número entre 1 e 100: ")
+    print("Você digitou", chute_str,)
     chute = int(chute_str)
+
+    if chute < 1 or chute > 100:
+        print("você deve digitar um nùmero entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -20,6 +23,7 @@ while rodada <= total_de_tentativas:
 
     if acertou:
         print("Você acertou o número secreto!")
+        break
     elif maior:
         print("Você errou.O seu chute foi maior que o número secreto")
     elif menor:
